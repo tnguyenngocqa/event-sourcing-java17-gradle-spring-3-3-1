8815a34535f19a8e76de4100f87ff6d0e2762eb4
tnguyenngocqa <tnguyenngocqa@gmail.com>
2026-03-05 20:48:02 +0700

    kafka config in common service

--- bookservice/src/main/java/com/ltfullstack/bookservice/query/controller/BookQueryController.java
+++ bookservice/src/main/java/com/ltfullstack/bookservice/query/controller/BookQueryController.java
@@ -3,13 +3,11 @@ package com.ltfullstack.bookservice.query.controller;
 import com.ltfullstack.bookservice.query.model.BookResponseModel;
 import com.ltfullstack.bookservice.query.queries.GetAllBooksQuery;
 import com.ltfullstack.bookservice.query.queries.GetBookDetailQuery;
+import com.ltfullstack.commonservice.service.KafkaService;
 import org.axonframework.messaging.responsetypes.ResponseTypes;
 import org.axonframework.queryhandling.QueryGateway;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -30,5 +28,14 @@ public class BookQueryController {
     public BookResponseModel getBook(@PathVariable("bookId") String bookId) {
         GetBookDetailQuery query = new GetBookDetailQuery(bookId);
         return queryGateway.query(query, ResponseTypes.instanceOf(BookResponseModel.class)).join();
+    }
+
+
+    @Autowired
+    private KafkaService kafkaService;
+
+    @PostMapping("/sendMessage")
+    public void sendMessage(@RequestBody String message) {
+        kafkaService.sendMessage("test", message);
     }
 }

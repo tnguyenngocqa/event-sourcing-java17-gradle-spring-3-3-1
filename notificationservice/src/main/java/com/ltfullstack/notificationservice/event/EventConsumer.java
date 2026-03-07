a24b9fc3d7527865904ce5c7dad344ef96c727c3
tnguyenngocqa <tnguyenngocqa@gmail.com>
2026-03-07 10:10:01 +0700

    Kafka Retry + Dead Letter Queue

--- notificationservice/src/main/java/com/ltfullstack/notificationservice/event/EventConsumer.java
+++ notificationservice/src/main/java/com/ltfullstack/notificationservice/event/EventConsumer.java
@@ -1,15 +1,35 @@
 package com.ltfullstack.notificationservice.event;
 
 import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.common.errors.RetriableException;
+import org.springframework.kafka.annotation.DltHandler;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.annotation.RetryableTopic;
+import org.springframework.kafka.retrytopic.DltStrategy;
+import org.springframework.retry.annotation.Backoff;
 import org.springframework.stereotype.Component;
 
 @Component
 @Slf4j
 public class EventConsumer {
 
+    @RetryableTopic(
+            attempts = "4", // 3 topic retry + 1 topic DLQ
+            backoff = @Backoff(delay = 1000, multiplier = 2),
+            autoCreateTopics = "true",
+            dltStrategy = DltStrategy.FAIL_ON_ERROR,
+            include = {RetriableException.class, RuntimeException.class}
+    )
     @KafkaListener(topics = "test", containerFactory = "kafkaListenerContainerFactory")
     public void listen(String message) {
         log.info("Received Message: {}", message);
+
+        // simulate error
+        // throw new RuntimeException("Test Exception in the KafkaListener @RetryableTopic");
+    }
+
+    @DltHandler
+    private void processDLQ(String message) {
+        log.info("Dlt Handler - Received Message: {}", message);
     }
 }

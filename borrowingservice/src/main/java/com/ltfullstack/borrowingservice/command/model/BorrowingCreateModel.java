d2f27877482e0216fb413fedc30e550547772672
tnguyenngocqa <tnguyenngocqa@gmail.com>
2026-03-09 08:58:33 +0700

    CreateBorrowing

---
+++ borrowingservice/src/main/java/com/ltfullstack/borrowingservice/command/model/BorrowingCreateModel.java
@@ -0,0 +1,15 @@
+package com.ltfullstack.borrowingservice.command.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class BorrowingCreateModel {
+    private String bookId;
+    private String employeeId;
+}
